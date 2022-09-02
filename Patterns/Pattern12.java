f1b99b113eb29c682c8691e980028b14fa6ef48b
Hardik Gaur <96662380+Hardyy00@users.noreply.github.com>
2022-09-02 22:30:23 +0530

    Add files via upload

---
+++ Patterns/Pattern12.java
@@ -0,0 +1,38 @@
+package patterns.com.hardik;
+
+import java.util.Scanner;
+
+public class Pattern12 {
+
+    public static void main(String[] args) {
+
+        Scanner cin = new Scanner(System.in);
+        int n = cin.nextInt();
+
+        int row = 1;
+        int space = n-1;
+        int star = 1;
+
+        while(row<=n){
+
+            int i=1;
+            while(i<=space){
+                System.out.print("   ");
+                i++;
+            }
+
+            i=1;
+            while(i<=star){
+                String s = i%2!=0 ? "*  " : "!  ";
+                System.out.print(s);
+                i++;
+            }
+
+            space--;
+            star +=2;
+            row++;
+            System.out.println();
+        }
+        cin.close();
+    }
+}
