c68d8fb3b5916b27124b054cb1b706b1c993fc1c
Hardik Gaur <96662380+Hardyy00@users.noreply.github.com>
2022-09-02 22:27:18 +0530

    Add files via upload

---
+++ Patterns/Pattern4.java
@@ -0,0 +1,38 @@
+package patterns.com.hardik;
+
+import java.util.Scanner;
+
+public class Pattern4 {
+
+    public static void main(String[] args) {
+
+        Scanner cin = new Scanner(System.in);
+        int n = cin.nextInt();
+
+        int row = 1;
+        int space = n-1;
+        int star = 1;
+
+        while(row<=n){
+
+            int i=1;
+            while(i<=space){
+                System.out.print("   ");
+                i++;
+            }
+
+            i=1;
+            while(i<=star){
+                System.out.print("*  ");
+                i++;
+            }
+
+            star++;
+            space--;
+            row++;
+            System.out.println();
+        }
+
+        cin.close();
+    }
+}
