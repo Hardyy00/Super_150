f2df61dd5fdc51bcb008ede29da7d0fb78990933
Hardik Gaur <96662380+Hardyy00@users.noreply.github.com>
2022-09-03 11:28:29 +0530

    Add files via upload

---
+++ Patterns/Pattern28.java
@@ -0,0 +1,46 @@
+package patterns.com.hardik;
+
+import java.util.Scanner;
+
+public class Pattern28 {
+
+    public static void main(String[] args){
+
+        Scanner cin = new Scanner(System.in);
+        int n = cin.nextInt();
+
+        int row = 1;
+        int column = 1;
+        int space = n-1;
+
+        while(row<=n){
+
+            int i=1;
+            while(i<=space){
+                System.out.print("   ");
+                i++;
+            }
+
+            i=1;
+            int k = row;
+            while (i<=column){
+
+                System.out.print(k + "  ");
+
+                if(i<=column/2){
+                    k++;
+                }else{
+                    k--;
+                }
+
+                i++;
+            }
+
+            space--;
+            column+=2;
+            row++;
+            System.out.println();
+        }
+        cin.close();
+    }
+}
