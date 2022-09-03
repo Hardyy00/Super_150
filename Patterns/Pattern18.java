1c7ded21f262c4602bd9c0c20c6845b4f5ea2cdd
Hardik Gaur <96662380+Hardyy00@users.noreply.github.com>
2022-09-03 11:25:38 +0530

    Add files via upload

---
+++ Patterns/Pattern18.java
@@ -0,0 +1,43 @@
+package patterns.com.hardik;
+
+import java.util.Scanner;
+
+public class Pattern18 {
+
+    public static void main(String[] args){
+
+        Scanner cin = new Scanner(System.in);
+        int n = cin.nextInt();
+
+        int row = 1;
+        int space = n/2;
+        int star = 1;
+
+        while (row<=n){
+
+            int i=1;
+            while (i<=space){
+                System.out.print("   ");
+                i++;
+            }
+
+            i=1;
+            while (i<=star){
+                System.out.print("*  ");
+                i++;
+            }
+
+            if(row<=n/2){
+                space--;
+                star+=2;
+            }else{
+                space++;
+                star-=2;
+            }
+
+            row++;
+            System.out.println();
+        }
+        cin.close();
+    }
+}
