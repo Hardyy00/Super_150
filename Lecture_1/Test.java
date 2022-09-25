bd9ce68f0e3256a15a32dc6efb154d5dd5afa68c
Hardik Gaur <96662380+Hardyy00@users.noreply.github.com>
2022-09-25 23:24:00 +0530

    Add files via upload

---
+++ Lecture_1/Test.java
@@ -0,0 +1,17 @@
+package Lec1;
+
+public class Test {
+
+	public static void main(String[] args) {
+		
+		System.out.println("Hello World");
+		
+		System.out.print("Hello World");
+		System.out.println("Hyy");
+		
+		
+		
+	}
+
+}
+                                                                                                                                                                                          
