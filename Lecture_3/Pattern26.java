3aaf0ffb483a5e0dd53c9257cc65bb7c4822fd6e
Hardik Gaur <96662380+Hardyy00@users.noreply.github.com>
2022-09-25 23:39:38 +0530

    Add files via upload

---
+++ Lecture_3/Pattern26.java
@@ -0,0 +1,43 @@
+package Lec3;
+
+import java.util.Scanner;
+
+public class Pattern26 {
+
+	public static void main(String[] args) {
+		
+Scanner cin = new Scanner(System.in);
+		
+		int n = cin.nextInt();
+		
+		int row = 1;
+		int star = 1;
+		int space = n-1;
+		
+		while(row<=n) {
+			
+			int i=1;
+			
+			while(i<=space) {
+				System.out.print("  ");
+				i++;
+			}
+			
+			
+			i=1;
+			
+			while(i<=star) {
+				System.out.print(i+" ");
+				i++;
+			}
+			
+			star += 2;
+			space--;
+			row++;
+			System.out.println();
+		}
+		
+
+	}
+
+}
