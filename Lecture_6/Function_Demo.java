8684982e15897b823590dff33a0e9722fbbc70d1
Hardik Gaur <96662380+Hardyy00@users.noreply.github.com>
2022-09-26 23:40:23 +0530

    Add files via upload

---
+++ Lecture_6/Function_Demo.java
@@ -0,0 +1,30 @@
+package Lec6;
+
+public class Function_Demo {
+
+	public static void main(String[] args) {
+		
+		
+		addition();
+
+	}
+	
+	public static void addition() {
+		
+		int a = 7;
+		int b = 8;
+		sub();
+		System.out.println(a+b);
+	}
+	
+	public static void sub() {
+		
+		int a = 7;
+		int b = 8;
+		System.out.println(a-b);
+		
+	}
+	
+	
+
+}
