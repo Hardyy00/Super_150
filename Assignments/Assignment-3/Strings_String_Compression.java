a884c04ef114c583549248ab9d7554ac30ae6217
Hardik Gaur <96662380+Hardyy00@users.noreply.github.com>
2022-11-09 22:06:33 +0530

    Add files via upload

---
+++ Assignments/Assignment-3/Strings_String_Compression.java
@@ -0,0 +1,37 @@
+import java.util.*;
+class Main {
+    public static void main(String args[]) {
+
+        Scanner cin = new Scanner(System.in);
+        String s = cin.nextLine();
+
+        char pre = s.charAt(0);
+        String ans = "" + pre;
+
+        int count=1;
+
+        for(int i=1; i<s.length();i++){
+
+            char curr = s.charAt(i);
+
+            if(curr == pre){
+                count++;
+            }else{
+
+                if(count>1)
+                    ans+= count;
+                
+                ans += curr;
+                count = 1;
+            }
+
+            pre = curr;
+        }
+
+        if(count>1)
+            ans+=count;
+
+        System.out.println(ans);
+        cin.close();
+    }
+}
